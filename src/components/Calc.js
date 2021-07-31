38d56e826bdb8f813cf6451fc27f4f914fbeafe8
tarun2506 <tarunsharma8920@gmail.com>
2021-07-31 21:05:52 +0530

    functionality

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
+import { evaluate } from "mathjs";
 import "../styles/calc.css";
 
 function Calc() {
   const [text, setText] = useState("");
   const [clearInput, setClearInput] = useState(false);
   const [error, setError] = useState("");
+  const [output, setOutput] = useState("");
 
   const handleClear = () => {
     setClearInput(true);
     setText("");
     setError("");
+    setOutput("");
   };
 
   const handleNumInput = (e) => {
@@ -23,8 +26,21 @@ function Calc() {
       }, 1000);
       setTimeout(() => {
         setClearInput(true);
+        setText("");
       }, 1500);
     }
+  };
+
+  const handleOutput = () => {
+    const output = evaluate(text);
+    if (output) {
+      setOutput(`= ${output}`);
+      const ele = document.getElementById("output");
+      const ele2 = document.getElementById("input");
+      ele.classList.add("output");
+      ele2.classList.add("input");
+    }
+    console.log(output);
   };
 
   return (
@@ -32,15 +48,16 @@ function Calc() {
       <div id="display">
         {error && <p id="error">{error}</p>}
         {clearInput ? <p id="input">0</p> : <p id="input">{text}</p>}
+        <p id="output">{output}</p>
       </div>
       <div id="buttons">
         <button id="clear" onClick={handleClear}>
           AC
         </button>
-        <button id="add" className="operator">
+        <button id="add" className="operator" onClick={handleNumInput}>
           +
         </button>
-        <button id="subtract" className="operator">
+        <button id="subtract" className="operator" onClick={handleNumInput}>
           -
         </button>
         <button id="seven" onClick={handleNumInput}>
@@ -52,7 +69,7 @@ function Calc() {
         <button id="nine" onClick={handleNumInput}>
           9
         </button>
-        <button id="multiply" className="operator">
+        <button id="multiply" className="operator" onClick={handleNumInput}>
           *
         </button>
         <button id="four" onClick={handleNumInput}>
@@ -64,8 +81,8 @@ function Calc() {
         <button id="six" onClick={handleNumInput}>
           6
         </button>
-        <button id="divide" className="operator">
-          ÷
+        <button id="divide" className="operator" onClick={handleNumInput}>
+          /
         </button>
         <button id="one" onClick={handleNumInput}>
           1
@@ -76,7 +93,7 @@ function Calc() {
         <button id="three" onClick={handleNumInput}>
           3
         </button>
-        <button id="multiply" className="operator">
+        <button id="multiply" className="operator" onClick={handleNumInput}>
           %
         </button>
         <button id="zero" onClick={handleNumInput}>
@@ -85,7 +102,9 @@ function Calc() {
         <button id="decimal" onClick={handleNumInput}>
           .
         </button>
-        <button id="equals">=</button>
+        <button id="equals" onClick={handleOutput}>
+          =
+        </button>
       </div>
     </div>
   );
