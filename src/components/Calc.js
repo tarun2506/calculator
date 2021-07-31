1abb74f397a77a2a26556b91d829210b339217cc
tarun2506 <tarunsharma8920@gmail.com>
2021-07-31 17:01:43 +0530

    10% functionality added

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -2,40 +2,86 @@ import React, { useState } from "react";
 import "../styles/calc.css";
 
 function Calc() {
-  const [text, setText] = useState(0);
+  const [text, setText] = useState("");
+  const [clearInput, setClearInput] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleClear = () => {
+    setClearInput(true);
+    setText("");
+    setError("");
+  };
+
+  const handleNumInput = (e) => {
+    setClearInput(false);
+    if (text.length <= 14) {
+      setText((prev) => prev + e.target.innerText);
+    } else {
+      setError("Limit Reached 😭");
+      setTimeout(() => {
+        setError("");
+      }, 1000);
+    }
+  };
+
   return (
     <div id="calculator">
       <div id="display">
-        <p>{text}</p>
+        {error && <p id="error">{error}</p>}
+        {clearInput ? <p id="input">0</p> : <p id="input">{text}</p>}
       </div>
       <div id="buttons">
-        <button id="clear">AC</button>
+        <button id="clear" onClick={handleClear}>
+          AC
+        </button>
         <button id="add" className="operator">
           +
         </button>
         <button id="subtract" className="operator">
           -
         </button>
-        <button id="seven">7</button>
-        <button id="eight">8</button>
-        <button id="nine">9</button>
+        <button id="seven" onClick={handleNumInput}>
+          7
+        </button>
+        <button id="eight" onClick={handleNumInput}>
+          8
+        </button>
+        <button id="nine" onClick={handleNumInput}>
+          9
+        </button>
         <button id="multiply" className="operator">
           *
         </button>
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
+        <button id="four" onClick={handleNumInput}>
+          4
+        </button>
+        <button id="five" onClick={handleNumInput}>
+          5
+        </button>
+        <button id="six" onClick={handleNumInput}>
+          6
+        </button>
         <button id="divide" className="operator">
           ÷
         </button>
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
+        <button id="one" onClick={handleNumInput}>
+          1
+        </button>
+        <button id="two" onClick={handleNumInput}>
+          2
+        </button>
+        <button id="three" onClick={handleNumInput}>
+          3
+        </button>
         <button id="multiply" className="operator">
           %
         </button>
-        <button id="zero">0</button>
-        <button id="decimal">.</button>
+        <button id="zero" onClick={handleNumInput}>
+          0
+        </button>
+        <button id="decimal" onClick={handleNumInput}>
+          .
+        </button>
         <button id="equals">=</button>
       </div>
     </div>
